903cdec4ee1080bd82f1920919fb81f23a2aefaa
Home <home@Homes-MacBook-Air.local>
2025-10-20 11:33:06 -0400

    Got LSQFit.py to make the 1000 n experiments

--- LSQFit.py
+++ LSQFit.py
@@ -16,12 +16,13 @@ def f(x,par):
     return par[0]+par[1]*log(x)+par[2]*log(x)*log(x)
 
 from random import gauss
-# this function is identital to np.arange()
+# this function is identital to np.arange(i, f, n)
 def getX(x):  # x = array-like
     step=(xmax-xmin)/npoints
     for i in range(npoints):
         x[i]=xmin+i*step
         
+# just f(x) plus a random gaussian error
 def getY(x,y,ey):  # x,y,ey = array-like
     for i in range(npoints):
         y[i]=f(x[i],pars)+gauss(0,sigma)
@@ -34,12 +35,14 @@ getY(lx,ly,ley)
 fig, ax = plt.subplots()
 ax.errorbar(lx, ly, yerr=ley)
 ax.set_title("Pseudoexperiment")
-fig.show()
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+# fig.show() # no need to show plot for now.
 
 
 # *** modify and add your code here ***
 nexperiments = 1000  # for example
-
+nPar = 3
 
 # perform many least squares fits on different pseudo experiments here
 # fill histograms w/ required data
@@ -48,6 +51,48 @@ par_a = np.random.rand(1000)   # simple placeholders for making the plot example
 par_b = np.random.rand(1000)   # these need to be filled using results from your fits
 par_c = np.random.rand(1000)
 chi2_reduced = np.random.rand(1000)
+
+# for each fit, do the following:
+for fit in range(nexperiments):
+    getX(lx)
+    getY(lx, ly, ley)
+    function_points = []
+    for i in range(npoints):
+        function_points.append(f(lx[i], pars)) #this is the function without gaussian errors
+    residuals = ly - function_points
+    residuals_squared = residuals * residuals
+    chi_square = sum(residuals_squared / (sigma * sigma)) # since all points have same sigma
+
+    ax = np.array(lx, dtype = 'd')
+    ax2 = lx*lx
+    ay = np.array(ly, dtype = 'd')
+    ae = np.array(ley, dtype = 'd')
+    nPnts = len(ax)
+
+    A = np.matrix(np.zeros((nPnts, nPar)))
+
+    # fill matrix
+
+    for nr in range(nPnts):
+        for nc in range(nPar):
+            if nc == 0: A[nr, nc] = 1
+            if nc == 1: A[nr, nc] = ax[nr]
+            if nc == 2: A[nr, nc] = ax2[nr]
+    for i in range(nPnts):
+        A[i] = A[i] / ae[i] # weight with errors
+    yw = (ay/ae).reshape(nPnts, 1)
+    theta = inv(np.transpose(A).dot(A)).dot(np.transpose(A)).dot(yw)
+
+    # transform theta from a jank 1x1 numpy matrix into a sensible python 3 element list.
+
+    theta_list = list(theta.A.flatten())
+    # make list from theta as flattened array
+
+    # now fill the placeholders.
+    par_a[fit] = theta_list[0]
+    par_b[fit] = theta_list[1]
+    par_c[fit] = theta_list[2]
+    chi2_reduced[fit] = chi_square
 
 fig, axs = plt.subplots(2, 2)
 plt.tight_layout()
