187e16986fa8ffb8adf5414cca0f8a4aaa3737a9
Home <home@Homes-MacBook-Air.local>
2025-10-20 20:33:00 -0400

    fixed ; error

--- LSQFit.cpp
+++ LSQFit.cpp
@@ -137,7 +137,7 @@ int main(int argc, char **argv){
       TMatrixDRow(yw,irow) += y(irow)/e(irow);
     }
     cout << "A weighted = ";
-    A.Print()
+    A.Print();
     cout << "y weighted = ";
     yw.Print();
   }
